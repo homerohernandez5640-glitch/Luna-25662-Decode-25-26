64dab8c6540426523c678dc1ebc168fa0c9055e1
Luna25662 <homero.hernandez5640@gmail.com>
2026-02-02 18:45:17 -0600

    Umm
    This is just Pedropathing, but we didn't have time to tune it for now. We'll path Pedro for later. Stay safe Pedro

---
+++ Constants.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.Encoder;
+import com.pedropathing.ftc.localization.constants.DriveEncoderConstants;
+import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Constants {
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(8.165);
+
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("upperRight")
+            .rightRearMotorName("lowerRight")
+            .leftRearMotorName("lowerLeft")
+            .leftFrontMotorName("upperLeft")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE);
+
+    public static DriveEncoderConstants localizerConstants = new DriveEncoderConstants()
+            .rightFrontMotorName("upperRight")
+            .rightRearMotorName("lowerRight")
+            .leftRearMotorName("lowerLeft")
+            .leftFrontMotorName("upperLeft")
+            .leftFrontEncoderDirection(Encoder.FORWARD)
+            .leftRearEncoderDirection(Encoder.FORWARD)
+            .rightFrontEncoderDirection(Encoder.FORWARD)
+            .rightRearEncoderDirection(Encoder.FORWARD)
+            .robotWidth(16)
+            .robotLength(14.8)
+            .forwardTicksToInches(0.3)
+            .strafeTicksToInches(0.7)
+            .turnTicksToInches(0.5);
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+
+    public static Follower createFollower(HardwareMap hardwareMap) {
+        return new FollowerBuilder(followerConstants, hardwareMap)
+                .driveEncoderLocalizer(localizerConstants)
+                .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
+                .build();
+    }
+}
--- TeleLuna.java
+++ TeleLuna.java
@@ -18,9 +18,6 @@ public class TeleLuna extends OpMode{
     DcMotor upperRight;
     DcMotor lowerLeft;
     DcMotor lowerRight;
-    DcMotor Luna;
-    Servo Jesus;
-    DcMotor intake;
 
 
     public void init(){
@@ -28,27 +25,19 @@ public class TeleLuna extends OpMode{
         upperRight = hardwareMap.dcMotor.get("upperRight");
         lowerLeft = hardwareMap.dcMotor.get("lowerLeft");
         lowerRight = hardwareMap.dcMotor.get("lowerRight");
-        intake = hardwareMap.dcMotor.get("intake");
-        Luna = hardwareMap.dcMotor.get("Luna");
-        Jesus = hardwareMap.servo.get("Jesus");
 
 
-
-
-        upperLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        lowerLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-
+        lowerRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        upperRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
     }
     public void loop() {
-        double forward = -0.8 *
+        double forward = 0.8 *
                 gamepad1.left_stick_y;
-        double strafe = -0.8 *
+        double strafe = 0.8 *
                 gamepad1.left_stick_x;
-        double turn = -0.8 *
+        double turn = 0.8 *
                 gamepad1.right_stick_x;
-        double spin = 1 *
-                gamepad2.left_stick_y;
 
 
         upperLeft.setPower
@@ -59,46 +48,12 @@ public class TeleLuna extends OpMode{
                 (forward + strafe - turn);
         lowerRight.setPower
                 (forward - strafe + turn);
-        Luna.setPower(spin);
-
-
-        if (gamepad2.x) {
-            Jesus.setPosition(70);
-        }
-
-
-        else {
-            Jesus.setPosition(0);
-        }
-
-
-        if (gamepad2.right_bumper) {
-            Luna.setPower(1);
-        }
-
-
-        else {
-            Luna.setPower(0);
-        }
-
-
-
-
-
-
-        if (gamepad2.a) {
-            intake.setPower(-1);
-        }
-
-
-        else {
-            intake.setPower(0);
-
-
-        }
 
 
     }
 
 
 }
+
+
+
